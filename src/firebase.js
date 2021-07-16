9b09552b4381ff033c4df868112418039c5b6a33
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-16 13:32:26 -0700

    :pencil2: typo fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseConfig = {
@@ -13,6 +13,5 @@ const firebaseConfig = {
 }
 
 firebase.initializeApp(firebaseConfig);
-firebase.firestore();
 
 export default firebase;
