65a7c7ae49e157da8c3a5eb1e6173c8303008ad6
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-16 17:06:58 -0700

    :construction: working on updating nav menu to including router links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,25 @@
 import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <Navbar expand="lg" bg="light" variant="light">
-    <Container>
-    <Navbar.Brand href="#home">Modern Fish</Navbar.Brand>
-    <Nav className="me-auto"> {/* effects text location */}
-      <Nav.Link href="#about">About</Nav.Link>
-      <Nav.Link href="#faq">FAQ</Nav.Link>
-    </Nav>
-    </Container>
+      <Container>
+        {/* <Navbar.Brand to="/">Modern Fish</Navbar.Brand>
+        <Nav className="me-auto">  effects text location 
+          <Nav.Link to="/about">About</Nav.Link>
+          <Nav.Link href="#faq">FAQ</Nav.Link> */}
+
+          <Link to="/">Modern Fish</Link>
+          <Nav className="me-auto">
+          <Link to="/about">About</Link>
+          <Link href="#faq">FAQ</Link>
+          {/* Guess ill be messing up the bootstrap styling to make this basic nav menu work for now. Removed */}
+        </Nav>
+      </Container>
   </Navbar>
+
+
   );
 }
 
