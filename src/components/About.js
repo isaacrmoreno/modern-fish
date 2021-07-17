c41a34974c60dffe2ddf5f6b1cb530f01dfb03b4
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-16 17:03:44 -0700

    :sparkles: add basic about page

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+
+function About() {
+  
+  return(
+    <p>Modern Fish aims to simplify the fishing experience surrounding equipment, setup, and species information to empower the public to get outside and fish! 🎣</p>
+  );
+}
+
+export default About;
