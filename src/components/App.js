db69109878b519fac73c8d3f641b6316dd6f6150
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-16 17:07:24 -0700

    :sparkles: added router to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
+import About from "./About";
 import Header from "./Header";
 import License from "./License";
+// import Sponsors from "./Sponsors";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
+    <Router>
       <Header/>
-      <License/>
+      <Switch>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route path="/">
+          <License/>
+        </Route>
+        {/* <Sponsors/> */}
+      </Switch>
+    </Router>
     </>
   );
 }
