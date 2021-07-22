509dd82a88abcf08590fecab5a6cdb885b7e5d84
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-22 11:16:24 -0700

    :lipstick: add headingLg to p tag

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,12 @@
 import { Container } from 'react-bootstrap'
+import utilStyles from '../styles/utils.module.css'
 
 function About() {
   
   return(
     <>
       <Container>
-        <p>Modern Fish aims to simplify the fishing experience surrounding equipment, setup, and species information to empower the public to get outside and fish! 🎣</p>
+        <p className={utilStyles.headingLg}>Modern Fish Oregon aims to simplify the fishing experience surrounding equipment, setup, and species information to empower the public to get outside and fish! 🎣</p>
       </Container>
     </>
   );
