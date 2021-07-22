92778ba13534a46d21fef8af4e1ef2f441289091
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-21 21:21:15 -0700

    :art: break down docs into smaller components

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -1,18 +1,25 @@
 import { Container, Col, Row } from 'react-bootstrap'
+import Gear from './Gear';
+import GearSetUp from './GearSetUp';
+import Habitat from './Habitat';
 import SideBar from './SideBar';
+import Technique from './Technique';
+import Fish from './Fish';
 
 function Docs() {
   return (
     <>
       <Container>
         <Row>
-          <Col sm={4}>
+          <Col sm={4} className="left-col">
             <SideBar/>
           </Col>
-          <Col sm={8}>
-            <h1>This is where The documentation body will go</h1>
-
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <Col sm={8} className="main-col">
+            <Fish/>
+            <Habitat/>
+            <Technique/>
+            <Gear/>
+            <GearSetUp/>
           </Col>
         </Row>
       </Container>
