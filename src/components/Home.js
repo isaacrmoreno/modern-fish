401ed879845fd5961db4dbcc0e05601acfec3a41
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-22 11:10:01 -0700

    :lipstick: add utilStyles module styling for practice

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,18 @@
 import { LinkContainer } from 'react-router-bootstrap';
-import { Container, Nav } from 'react-bootstrap';
+import { Card, Container, Nav } from 'react-bootstrap';
+import utilStyles from '../styles/utils.module.css'
 
 function Home() {
   return (
     <>
     <Container>
-    <h1>Modern Fish</h1>
-        <Nav className="me-auto"> {/* effects text location  */}
-          <LinkContainer to="/docs/">
-            <Nav.Link>Fish Documentation 🎣</Nav.Link>
-          </LinkContainer>
+    <h1 className={utilStyles.heading2Xl}>Modern Fish</h1>
+        <Nav className="me-auto">
+            <Card border="primary" body>
+              <LinkContainer to="/docs/">
+                    <Nav.Link className={utilStyles.headingXl}>Fish Documentation 🎣</Nav.Link>
+              </LinkContainer>
+            </Card>
         </Nav>
     </Container>
     </>
