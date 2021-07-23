f9fa1c5bbfd739c8daf0d567ec5e95cce83303c2
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-23 12:48:23 -0700

    :construction: WIP: added data to firestore db

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,24 +1,87 @@
-class FishControl extends React.Component {
+import firebase from '../firebase';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-  constructor(props) {
-    super(props);
-    console.log(props);
-    this.state = {
-      selectedFish: null
-    };
+function Fish() {
+  
+  const ref = firebase.firestore().collection('Fish');
+
+  const [fish, setFishes] = useState([]);
+  const [selectedFish, setSelectedFish] = useState(null)
+
+  console.log(`selected Fish:`, selectedFish)
+
+  function getFishDetails() {
+    ref.onSnapshot((querySnapshot) => {
+      const fishDetails = [];
+      querySnapshot.forEach((doc) => {
+        fishDetails.push(doc.data());
+      })
+      setSelectedFish(fishDetails);
+    })
   }
 
-  handleClick = (id) => {
-    if (this.state.selectedFish != null) {
-      this.setState({
-        selectedTicket: id,
-      }); 
-    } else {
-      const { dispatch } = this.props;
-      const action = a.toggleForm();
-      dispatch(action)
-    }
+  useEffect(() => {
+    getFishDetails();
+  },[]);
+
+  const handleSelectFish = (id) => {
+    this.props.firestore.get({ collection: 'Fish', doc: id }).then((fish) => {
+      const firestoreFish = {
+        name: fish.get('name'),
+        features: fish.get('features'),
+        habitat: fish.get('habitat'),
+        technique: fish.get('technique'),
+        id: fish.id
+      }
+      setSelectedFish(firestoreFish);
+    })
   }
 
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   handleChange()    
+  //   return 
+  // } 
+    
+  const handleChange = e => {
+    console.log(`e.target:`,e.target)
+    const { value, name } = e.target
+    const selectedFishDetails = { ...selectedFish }
+    selectedFishDetails[name] = value
+    console.log(`value:`,value)
+    setSelectedFish(selectedFishDetails)
+  }
 
+  return (
+    <>
+      <Container>
+        <Row>
+            <Col>
+              <ul>
+              {fish.map((fish, index) => (
+                <li onClick={handleChange} key={index} value={fish.id}>
+                  <a href="/docs/:id">
+                    {fish.name}
+                  </a>
+                </li>
+                ))}
+              </ul> 
+
+              {/* I need to do the following:
+              - be able to click one of these li tags
+              - have each have an id associated with it
+              - Based off its id it will retrieve/display x,y,z information from firestore on mainContent component.
+              */}
+
+              {/* dynamic routing based of the fish id. */}
+              
+
+            </Col>
+        </Row>
+      </Container>
+  </>
+  );
 }
+
+export default Fish;
