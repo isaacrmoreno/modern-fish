d663196e60a14c5c04f80f0c2cbe5d42c51cc16b
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-23 17:02:26 -0700

    :fire: end of day commit

---
+++ src/components/DocContent.js
@@ -0,0 +1,47 @@
+import Fish from "./Fish";
+
+function DocContent(props) {
+  return (
+    <>
+      <h1>Getting Started</h1>
+
+      <hr />
+      {/* This is where I can put basic gear/technique info before they select a fish species to learn about*/}
+      {/* <License/> */}
+      <h1>{Fish.name}</h1>
+      <hr />
+      <h1>Features:</h1>
+      <p>{Fish.features}</p>
+      <h1>Habitat:</h1>
+      <p>{Fish.habitat}</p>
+      <h1>Technique:</h1>
+      <p>{Fish.technique}</p>
+    </>
+
+    //   <ul>
+    //   <li>
+    //     <Accordion>
+    //       <Accordion.Toggle as={Button} variant="link" eventKey="0">
+    //         Trout ↓
+    //       </Accordion.Toggle>
+    //       <Accordion.Collapse eventKey="0">
+    //         <p>Hello! I'm the body</p>
+    //       </Accordion.Collapse>
+    //     </Accordion>
+    //   </li>
+    //   <li>
+    //     <Accordion>
+    //       <Accordion.Toggle as={Button} variant="link" eventKey="1">
+    //         Steelhead ↓
+    //       </Accordion.Toggle>
+    //       <Accordion.Collapse eventKey="1">
+    //         <p>Hello! I'm the body</p>
+    //       </Accordion.Collapse>
+    //     </Accordion>
+    //   </li>
+
+    // </ul>
+  );
+}
+
+export default DocContent;
