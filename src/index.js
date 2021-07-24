dba58053b1c5f20425fd869df051fbdcb9331657
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-23 17:02:04 -0700

    :fire: small refactor

--- src/index.js
+++ src/index.js
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './styles/utils.module.css'
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
+        <App />, 
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
