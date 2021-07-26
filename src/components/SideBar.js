165f470682e5e1c9a505549cf7dbe6e54541c9d1
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-26 11:08:15 -0700

    :lipstick: added scrollbar for sideBar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,12 @@
 import Fish from './Fish'
 import { Container, Row } from 'react-bootstrap';
+import utilStyles from '../styles/utils.module.css'
 
 function SideBar() {
   return(
     <>
-      <Container>
-        <Row>
+      <Container className={utilStyles.container}>
+        <Row className={utilStyles.scrollBar}>
           <h3>Fish Species</h3>
           <hr/>
           <Fish/>
