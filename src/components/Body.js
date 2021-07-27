9e15f48ba6354a16d6b43b325862cb2805a36374
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-27 16:47:21 -0700

    :construction: end of day commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,18 +8,18 @@ import { Container, Col, Row } from 'react-bootstrap'
 
 function Body() {
 
-  let db = firebase.firestore();
+//   let db = firebase.firestore();
 
-  db.collection("fish").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`Firestore info:${doc.id} => ${doc.data()}`);
-    });
-});
+//   db.collection("fish").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(`Firestore info:${doc.id} => ${doc.data()}`);
+//     });
+// });
 
   const ref = firebase.firestore().collection("Fish");
 
   const [fish, setFishes] = useState([]);
-  const [selectedFish, setSelectedFish] = useState([]);
+  const [selectedFish, setSelectedFish] = useState(null);
   
 
   function getFishNames() {
@@ -36,37 +36,37 @@ function Body() {
     getFishNames();
   },[]);
 
-  const handleChangeSelectedFish = (id) => {
-    console.log(`id:`,id)
-    setSelectedFish(id)
-  }
+  // const handleChangeSelectedFish = (id) => {
+  //   console.log(`id:`,id)
+  //   setSelectedFish(id)
+  // }
 
   // this.props.firestore.get({collection: 'Fish', doc: id}).then(
 
-
-// const viewFishDetails = id => {
-//   firebase.firestore.get({collection: 'Fish', doc: id}).then(
-//     fish => {
-//       const firestoreFish ={
-//         name: fish.get('name'),
-//         features: fish.get('features'),
-//         habitat: fish.get('habitat'),
-//         technique: fish.get('technique')
-//       }
-//       setSelectedFish(firestoreFish)
-//     }
-//   )
-// }
+ const viewFishDetails = id => {
+  this.props.firestore.get({collection: 'Fish', doc: id}).then(
+    fish => {
+      const firestoreFish = {
+        name: fish.get('name'),
+        features: fish.get('features'),
+        habitat: fish.get('habitat'),
+        technique: fish.get('technique'),
+        id: fish.id
+      }
+      setSelectedFish(firestoreFish)
+    }
+  )
+}
 
   return (
     <>
       <Container>
         <Row>
           <Col sm={4}>
-            <SideBar fish={fish} onChangeFish={handleChangeSelectedFish}/>
+            <SideBar fish={fish} onChangeFish={viewFishDetails}/>
           </Col>
           <Col sm={8}>
-            <Details fish={fish} selectedFish={selectedFish} onChangeFish={handleChangeSelectedFish}/>
+            <Details fish={fish} selectedFish={selectedFish} onChangeFish={viewFishDetails}/>
           </Col>
         </Row>
       </Container>
